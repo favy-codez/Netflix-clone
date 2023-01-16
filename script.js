e7e3cc28f207ad27b5fc97a7ccb011e823fb8e68
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-01-16 01:54:39 +0100

    commit message

--- script.js
+++
@@ -1,8 +0,0 @@
-
-// // FAQ section
-// const FAQs = document.querySelectorAll('.FAQ');
-// FAQs.forEach(FAQ =>{
-//     FAQ.addEventListener('click', function() {
-//         FAQ.classList.toggle('showFAQ')
-//     })
-// })

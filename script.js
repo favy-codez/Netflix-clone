b64797c959b67348fdcddefab13e86c53d5ea9c8
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-01-23 09:04:22 +0100

    commit message

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const navToggle = document.querySelector(".nav-toggle");
-const links = document.querySelector(".links");
+// const navToggle = document.querySelector(".nav-toggle");
+// const links = document.querySelector(".links");
 
-navToggle.addEventListener("click", function () {
-  links.classList.toggle("show-links");
-});
+// navToggle.addEventListener("click", function () {
+//   links.classList.toggle("show-links");
+// });
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
